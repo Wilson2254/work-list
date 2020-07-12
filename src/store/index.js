7d155f0c0e561f6f23c89995b7389b58b6d853e8
Alexander_P <sanek191296@mail.ru>
2020-07-12 23:56:09 +0300

    Debug Delete button

--- src/store/index.js
+++ src/store/index.js
@@ -8,39 +8,43 @@ export default new Vuex.Store({
     tasks: JSON.parse(localStorage.getItem('tasks') || '[]'),
   },
   mutations: {
-    createTask(state, task){
+    createTask(state, task) {
       state.tasks.push(task)
       localStorage.setItem('tasks', JSON.stringify(state.tasks))
     },
-    updateTask(state, {id, description}){
+    updateTask(state, { id, description }) {
       const tasks = state.tasks.concat()
 
-      const idx = tasks.findIndex (t => t.id === id)
+      const idx = tasks.findIndex(t => t.id === id)
       const task = tasks[idx]
 
       state.tasks = tasks
       localStorage.setItem('tasks', JSON.stringify(state.tasks))
 
-      tasks[idx] = {...task, description}
+      tasks[idx] = { ...task, description }
     },
-    deleteTask(state){
-      localStorage.removeItem('tasks', JSON.stringify(state.tasks))
+    deleteTask(state, id) {
+      const tasks = state.tasks.concat()
+      const idx = tasks.findIndex(t => t.id === id)
+      tasks.splice(idx, 1);
+      state.tasks = tasks
+      localStorage.setItem('tasks', JSON.stringify(state.tasks));
     }
   },
   actions: {
-    createTask({commit}, task){
+    createTask({ commit }, task) {
       commit('createTask', task)
     },
-    updateTask ({commit}, task){
+    updateTask({ commit }, task) {
       commit('updateTask', task)
     },
-    deleteTask ({commit}, task){
+    deleteTask({ commit }, task) {
       commit('deleteTask', task)
     }
   },
   modules: {
   },
-  getters:{
+  getters: {
     tasks: s => s.tasks,
     taskById: s => id => s.tasks.find(t => t.id === id)
   }
